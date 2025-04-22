125b1faa483faf52f47b085b94c4590a37fe142d
Mateusz <m.kacprowicz98@gmail.com>
2025-04-22 23:46:30 +0200

    Added Book DTO and mapper

---
+++ Dtos/Book/BookDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreApi.Models;
+
+namespace BookStoreApi.Dtos.Book
+{
+    public class BookDto
+    {
+        public ulong Isbn { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Status { get; set; } = BookStatus.Status.Available;
+    }
+}
---
+++ Mappers/BookMappers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreApi.Dtos.Book;
+using BookStoreApi.Models;
+
+namespace BookStoreApi.Mappers
+{
+    public static class BookMappers
+    {
+        public static BookDto ToBookDto(this Book bookModel)
+        {
+            return new BookDto
+            {
+                Isbn = bookModel.Isbn,
+                Title = bookModel.Title,
+                Author = bookModel.Author,
+                Status = bookModel.Status
+            };
+        }
+    }
+}
